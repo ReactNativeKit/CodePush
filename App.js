dd6e768ebdf5223067a8d44ff0a60b47c30c3161
abhinavanand <abhi9vanand@gmail.com>
2022-02-10 16:13:22 +0530

    App.js updated

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ class App extends React.Component {
    render() {
     return (
       <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#FF5C8D'}}>
-      <Text style={{color:'#FDEFF4'}}>
+      <Text style={{fontSize:20, color:'#FDEFF4'}}>
         Hello! I am a react native app.....
       </Text>
-      <Text style={{color:'#8BDB81'}}>
+      <Text style={{fontSize:20, color:'#8BDB81'}}>
         Welcome to CodePush......
       </Text>
       </View>
